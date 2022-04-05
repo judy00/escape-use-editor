9e12d45a1e6abf0b7ffa4fd3f0fdb0a6248ba89f
JudyWu <judywu@sat.cool>
2022-04-05 17:33:08 +0800

    feat: 新增 Quill Image Uploader 功能

--- all.js
+++ all.js
@@ -1,10 +1,9 @@
-var quill = new Quill('#editor', {
+Quill.register("modules/imageUploader", ImageUploader);
+
+const quill = new Quill('#editor', {
   theme: 'snow',
   placeholder: '寫下產品描述',
   modules: {
-    imageResize: {
-      displaySize: true
-    },
     toolbar: {
       container: [
         [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
@@ -12,17 +11,32 @@ var quill = new Quill('#editor', {
         ['link', 'image'],
         ['clean']
       ],
-      handlers: {
-        image: imageHandler
+      // handlers: {
+      //   image: imageHandler
+      // }
+    },
+    imageResize: {
+      displaySize: true
+    },
+    imageUploader: {
+      upload: file => {
+        return new Promise((resolve, reject) => {
+          console.log('file :>> ', file);
+          setTimeout(() => {
+            resolve(
+              "https://images.unsplash.com/photo-1486427944299-d1955d23e34d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            );
+          }, 1000);
+        });
       }
     }
   },
 });
 
-function imageHandler() {
-  var range = this.quill.getSelection();
-  var value = prompt('請貼上圖片連結');
-  if (value) {
-    this.quill.insertEmbed(range.index, 'image', value, Quill.sources.USER);
-  }
-}
+// function imageHandler() {
+//   var range = this.quill.getSelection();
+//   var value = prompt('請貼上圖片連結');
+//   if (value) {
+//     this.quill.insertEmbed(range.index, 'image', value, Quill.sources.USER);
+//   }
+// }
