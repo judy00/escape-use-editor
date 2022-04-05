98c30474000e51c31d935ad09646f03606179ded
JudyWu <judywu@sat.cool>
2022-04-05 17:37:39 +0800

    refactor: 移除預設內容

--- all.js
+++ all.js
@@ -8,6 +8,8 @@ const quill = new Quill('#editor', {
       container: [
         [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
         ['bold', 'italic', 'underline', 'strike'],
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'color': [] }, { 'background': [] }],
         ['link', 'image'],
         ['clean']
       ],
