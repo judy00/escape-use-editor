77640421bf9ff34d199ee18dd6a285562ed6fa56
JudyWu <judywu@sat.cool>
2022-04-05 16:59:01 +0800

    feat: 新增圖片 resize 功能

--- all.js
+++ all.js
@@ -2,6 +2,9 @@ var quill = new Quill('#editor', {
   theme: 'snow',
   placeholder: '寫下產品描述',
   modules: {
+    imageResize: {
+      displaySize: true
+    },
     toolbar: [
       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
       ['bold', 'italic', 'underline', 'strike'],
