fc1e8466d445ce1c53125a1c5193856bff902ffb
JudyWu <judywu@sat.cool>
2022-04-05 15:52:19 +0800

    feat: 新增編輯器

---
+++ all.js
@@ -0,0 +1,12 @@
+var quill = new Quill('#editor', {
+  theme: 'snow',
+  placeholder: '寫下產品描述',
+  modules: {
+    toolbar: [
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      ['bold', 'italic', 'underline', 'strike'],
+      ['link', 'image'],
+      ['clean']
+    ]
+  },
+});
